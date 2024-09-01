a2a0793a0761df69b07804db4e6a913d74db95e4
Willis Plummer <willisplummer@gmail.com>
2024-08-31 23:15:47 -0600

    readme, a build etc

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# farm-to-table
+# Farm To Table
+
 Template is [here](https://github.com/willisplummer/ooga-booga-raylib)
 
-## folder structure
+## Project Structure
+
 - lib - libraries (mainly raylib 5.0 built libs)
 - src - code
 - assets - textures, audio, shaders, etc
 - bin - built application
 
+## Build
+
+run `make` in the top-level directory then you can play by calling the executable with `bin/build_mac`
 
 
--- src/main.c
+++ src/main.c
@@ -1,17 +1,53 @@
-#include <raylib.h>
-#include <raymath.h>
+#include "raylib.h"
 
-int main(void) {
-  InitWindow(400, 224, "Template");
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const char gameTitle[] = "Farm To Table";
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-      ClearBackground(RAYWHITE);
+    InitWindow(screenWidth, screenHeight, gameTitle);
 
-    EndDrawing();
-  }
+    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
 
-  CloseWindow();
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
 
-  return 0;
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+
+            DrawCircleV(ballPosition, 50, MAROON);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
+

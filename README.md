763d8da8a6677c9fe28a011237831b81092d852d
Willis Plummer <willisplummer@gmail.com>
2024-09-01 09:35:52 -0600

    render character and move him around

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Template is [here](https://github.com/willisplummer/ooga-booga-raylib)
 
+[Raylib cheatsheet](https://www.raylib.com/cheatsheet/cheatsheet.html)
+
 ## Project Structure
 
 - lib - libraries (mainly raylib 5.0 built libs)
@@ -13,4 +15,9 @@ Template is [here](https://github.com/willisplummer/ooga-booga-raylib)
 
 run `make` in the top-level directory then you can play by calling the executable with `bin/build_mac`
 
+## Next Steps
+- set background color
+- import my player asset
+- implement player movement
+- make sure I have a working debugger with nvim (and generally I have LSP for C etc)
 
--- src/main.c
+++ src/main.c
@@ -10,10 +10,12 @@ int main(void)
     const char gameTitle[] = "Farm To Table";
     const int screenWidth = 800;
     const int screenHeight = 450;
+    Color background = GetColor(0x4b692fff);
 
     InitWindow(screenWidth, screenHeight, gameTitle);
+    Texture2D playerTexture = LoadTexture("assets/player.png");
 
-    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    Vector2 playerPosition = { (float)screenWidth/2, (float)screenHeight/2 };
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -23,21 +25,21 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        if (IsKeyDown(KEY_RIGHT)) playerPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) playerPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) playerPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) playerPosition.y += 2.0f;
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(background);
 
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+            DrawText("move the player with arrow keys", 10, 10, 20, DARKGRAY);
 
-            DrawCircleV(ballPosition, 50, MAROON);
+            DrawTextureV(playerTexture, playerPosition, RAYWHITE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
